c02ee65cd118e32bfa274c06b3925e3088c1a69f
sdgillen <sdgillen@crimson.ua.edu>
2017-11-25 20:58:15 -0600

    Remove record controllers and add interactive mode class

---
+++ src/team11project4/InteractiveMode.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package team11project4;
+public class InteractiveMode {
+	
+	ChocAnDataCenter db;
+
+	/**
+	 * 
+	 */
+	public InteractiveMode() {
+		db = new ChocAnDataCenter();
+	}
+
+}
