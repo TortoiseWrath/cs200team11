625d5521d8853fd494cd25f8222fcbbab134979f
sdgillen <sdgillen@crimson.ua.edu>
2017-11-25 18:48:56 -0600

    Terminal class stub

---
+++ src/team11project4/Terminal.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package team11project4;
+
+/**
+ * @author sdgillen
+ *
+ */
+public class Terminal {
+
+	/**
+	 * 
+	 */
+	public Terminal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
