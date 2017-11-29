8ad790efb864ef0d134de34c394ca8d59831473d
jhallen3 <jhallen3@crimson.ua.edu>
2017-11-28 18:57:39 -0600

    Added constructor for Member class;

--- src/team11project4/Member.java
+++ src/team11project4/Member.java
@@ -1,12 +1,11 @@
 package team11project4;
 
 public class Member {
-	
+
 	public enum MemberStatus {
-		ACTIVE,
-		SUSPENDED
+		ACTIVE, SUSPENDED
 	}
-	
+
 	public String memberNumber;
 	public String memberName;
 	public MemberStatus memberStatus;
@@ -15,8 +14,27 @@ public class Member {
 	public String memberState;
 	public String memberZip;
 
-	public Member() {
-		// TODO Auto-generated constructor stub
+	/**
+	 * Constructor for creating new a new member. Requires a string for each of the member's name,
+	 * address, city, state, and zip. Also requires a MemberStatus type for the member's status.
+	 * 
+	 * @param number	A string representing the member's unique number
+	 * @param name		A string representing the member's name
+	 * @param address	A string representing the member's address
+	 * @param city		A string representing the member's city
+	 * @param state		A string representing the member's state
+	 * @param zip		A string representing the member's zip code
+	 * @param status	The status of the member. Can be either MemberStatus.ACTIVE or MemberStatus.SUSPENDED
+	 */
+	public Member(String number, String name, String address, String city, String state, String zip,
+			MemberStatus status) {
+		this.memberNumber = number;
+		this.memberName = name;
+		this.memberAddress = address;
+		this.memberCity = city;
+		this.memberState = state;
+		this.memberZip = zip;
+		this.memberStatus = status;
 	}
 
 }
