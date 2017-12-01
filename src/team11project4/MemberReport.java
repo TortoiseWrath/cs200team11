0fac492d6d2ad02fda3a2e7ac20f786d1303acf2
bphightower <bphightower@crimson.ua.edu>
2017-12-01 13:56:59 -0600

    MemberReport Added

--- src/team11project4/MemberReport.java
+++ src/team11project4/MemberReport.java
@@ -1,11 +1,33 @@
 package team11project4;
 
+import java.util.ArrayList;
+
 public class MemberReport {
 	
 	public String memberReport;
 
 	public MemberReport() {
-		// TODO Auto-generated constructor stub
+		//Clear previous report
+		memberReport = "";
+		String temp;
+		ChocAnDataCenter Center = null;
+		ArrayList<Member> memberList = Center.getMemberData();
+		ArrayList<ProvidedServiceRecord> provServiceList = null;
+		//Iterate through members
+		
+		for (Member m: memberList) {
+			temp = "";
+			temp += m.memberNumber + " " + m.memberName + " " + m.memberAddress + " " + m.memberCity " " + m.memberState " " + m.memberZip + " ";
+			temp += "Status: " m.memberStatus;
+			for (ProvidedServiceRecord p: provServiceList) {
+				temp += "providedService: " + p.serviceCode + " ";
+				temp += "provider: " + p.providerNumber + " ";
+				temp += "on: " + p.dateProvided;
+				temp += "for: $" + p.fee + " ";
+				temp += "posted: " + p.dateAdded + "\n";
+				temp += "-----comments: " + p.comments + "\n";
+			}
+			memberReport += temp;
+		}
 	}
-
 }
