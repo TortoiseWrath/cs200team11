861a5f1c7f36ca55303157a10a87c9a16ce39ed5
sdgillen <sdgillen@crimson.ua.edu>
2017-12-01 22:30:22 -0600

    more test cases

--- src/team11project4/ChocAnDataCenterTest.java
+++ src/team11project4/ChocAnDataCenterTest.java
@@ -24,6 +24,8 @@ public class ChocAnDataCenterTest {
 		assertEquals(db.getMemberData().size(), 0);
 		assertEquals(db.getProviderData().size(), 0);
 	}
+	
+	
 
 	@Test
 	public final void testGetServiceData() {
@@ -40,6 +42,8 @@ public class ChocAnDataCenterTest {
 		fail("Not yet implemented");
 	}
 
+	
+	
 	@Test
 	public final void testGetEFTData() {
 		fail("Not yet implemented");
@@ -50,16 +54,8 @@ public class ChocAnDataCenterTest {
 		fail("Not yet implemented");
 	}
 
-	@Test
-	public final void testUpdateProviderProviderProvider() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public final void testUpdateProviderProvider() {
-		fail("Not yet implemented");
-	}
-
+	
+	
 	@Test
 	public final void testAddGetMember() {
 		Member m = new Member();
@@ -68,6 +64,45 @@ public class ChocAnDataCenterTest {
 		db.addMember(m);
 		assertEquals("test member", db.getMember("67").memberName);
 	}
+
+	@Test
+	public final void testUpdateMemberMemberMember() {
+		testAddGetMember();
+		Member m1 = db.getMember("67");
+		Member m2 = m1;
+		m2.memberZip = "00109";
+		db.updateMember(m1, m2);
+		assertEquals("00109", db.getMember("67").memberZip);
+		
+		//also test updating the member number
+		m2.memberNumber = "42";
+		db.updateMember(m1, m2);
+		assertEquals("00109", db.getMember("42").memberZip); //the new member exists
+		assertEquals(null, db.getMember("67")); //the old member does not
+	}
+
+	@Test
+	public final void testUpdateMemberMember() {
+		testAddGetMember();
+		Member m = db.getMember("67");
+		m.memberZip = "V0H 1H0";
+		db.updateMember(m);
+		assertEquals("V0H 1H0", db.getMember("67").memberZip);
+		
+		//also test update the member number - should fail
+		m.memberNumber = "58";
+		assertEquals(db.updateMember(m), false);
+	}
+
+	@Test
+	public final void testDeleteMember() {
+		testAddGetMember();
+		Member m = db.getMember("67");
+		db.deleteMember(m);
+		assertEquals(db.getMember("67"), null);
+	}
+	
+	
 
 	@Test
 	public final void testAddGetProvider() {
@@ -79,12 +114,44 @@ public class ChocAnDataCenterTest {
 	}
 
 	@Test
+	public final void testUpdateProviderProviderProvider() {
+		testAddGetProvider();
+		Provider p1 = db.getProvider("67");
+		Provider p2 = db.getProvider("67");
+		p2.providerZip = "99150";
+		db.updateProvider(p1, p2);
+		assertEquals(db.getProvider("67").providerZip, "99150");
+		
+		//also test updating the provider number
+		p2.providerNumber = "102";
+		db.updateProvider(p1, p2);
+		assertEquals(db.getProvider("102").providerZip, "99150"); //the new provider exists
+		assertEquals(db.getProvider("67"), null); //and the old one does not
+	}
+
+	@Test
+	public final void testUpdateProviderProvider() {
+		testAddGetProvider();
+		Provider p = db.getProvider("67");
+		p.providerZip = "35487";
+		db.updateProvider(p);
+		assertEquals(db.getProvider("67").providerZip, "35487");
+		
+		//also test updating the provider number - should fail
+		p.providerNumber = "4";
+		db.updateProvider(p);
+		assertEquals(db.updateProvider(p), false);
+	}
+
+	@Test
 	public final void testDeleteProvider() {
 		testAddGetProvider();
 		Provider p = db.getProvider("67");
 		db.deleteProvider(p);
 		assertEquals(db.getProvider("67"), null);
 	}
+	
+	
 	
 	@Test
 	public final void testAddGetService() {
@@ -96,30 +163,19 @@ public class ChocAnDataCenterTest {
 	}
 	
 	@Test
-	public final void testPersistence() {
-		testAddGetMember();
-		db = new ChocAnDataCenter("testDB"); //read the file
-		assertEquals("test member", db.getMember("67"));
-	}
-
-	@Test
-	public final void testUpdateMemberMemberMember() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public final void testUpdateMemberMember() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public final void testDeleteMember() {
+	public final void testDeleteService() {
+		testAddGetService();
+		Service s = db.getService("67");
+		db.deleteService(s);
+		assertEquals(db.getService("67"), null);
 		fail("Not yet implemented");
 	}
 	
 	@Test
-	public final void testDeleteService() {
-		fail("Not yet implemented");
+	public final void testPersistence() {
+		testAddGetMember();
+		db = new ChocAnDataCenter("testDB"); //read the file
+		assertEquals("test member", db.getMember("67").memberName);
 	}
 
 }
