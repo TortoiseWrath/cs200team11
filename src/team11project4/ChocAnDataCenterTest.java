85e85f0f589eaef0b04e76955bc086964d90cf3c
sdgillen <sdgillen@crimson.ua.edu>
2017-12-01 21:11:21 -0600

    JUnit test stubs

---
+++ src/team11project4/ChocAnDataCenterTest.java
@@ -0,0 +1,109 @@
+package team11project4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ChocAnDataCenterTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public final void testChocAnDataCenter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testChocAnDataCenterString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testClear() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetService() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetMember() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetProvider() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetServiceData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetMemberData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetProviderData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetEFTData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testWriteEFTReport() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testAddProvider() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testUpdateProviderProviderProvider() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testUpdateProviderProvider() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testDeleteProvider() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testAddMember() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testUpdateMemberMemberMember() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testUpdateMemberMember() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testDeleteMember() {
+		fail("Not yet implemented");
+	}
+
+}
