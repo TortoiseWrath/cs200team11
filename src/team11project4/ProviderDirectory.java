50545fb0e93e12be773ea5f6ab99ad2f5d764689
jhallen3 <jhallen3@crimson.ua.edu>
2017-12-04 20:30:15 -0600

    Added constructor for provider directory

--- src/team11project4/ProviderDirectory.java
+++ src/team11project4/ProviderDirectory.java
@@ -1,9 +1,15 @@
 package team11project4;
 
 public class ProviderDirectory {
+	
+	public String directory = "Services:\n\n";
 
-	public ProviderDirectory() {
-		// TODO Auto-generated constructor stub
+	public ProviderDirectory(ChocAnDataCenter db) {
+		for (ChocAnService s : db.dummyServices) {
+			directory += s.serviceName + "/n";
+			directory += "\tCode: " + s.serviceCode + "\n";
+			directory += "\tFee: " + s.serviceFee + "\n\n";
+		}
 	}
 
 }
