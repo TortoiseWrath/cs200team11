bd590143be1d296d03c89a2c62dee86360aa8c69
bphightower <bphightower@crimson.ua.edu>
2017-12-07 12:25:34 -0600

    Reformatted ProviderTest class so there's 3 seperate tests

--- src/team11project4/ProviderTest.java
+++ src/team11project4/ProviderTest.java
@@ -10,11 +10,16 @@ import org.junit.Test;
 public class ProviderTest {
 
 	@Test
-	public final void testEqualsprovider() {
+	public final void sanityTest1() {
 		Provider m1 = new Provider();
-		Provider m2 = new Provider(m1); //make sure you can copy empty providers
-		assertTrue(m1.equals(m2));
-		
+		Provider m2 = new Provider(m1);
+		assertTrue(m1.equals(m2)); //Just making sure both methods create Provider
+	}
+	
+	@Test
+	public final void failCheck() {
+		Provider m1 = new Provider();
+		Provider m2 = new Provider(m1);
 		m1.providerAddress = "provider Address";
 		m1.providerCity = "provider City";
 		m1.providerName = "provider Name";
@@ -25,14 +30,18 @@ public class ProviderTest {
 		m1.providedServices.add(new ProvidedServiceRecord(new Date(),"3","0003","30303","",4));
 		m1.providedServices.add(new ProvidedServiceRecord(new Date(),"33","0003","30305","",4));
 		assertFalse(m1.equals(m2)); //make sure they are not the same now
-		
-		m2 = new Provider(m1);
-		m2.providedServices.remove(1);
-		assertFalse(m1.equals(m2)); //make sure they are not the same anymore
-		
-		m2.providedServices.add(m1.providedServices.get(1)); //move the second service to the end
-		assertTrue(m1.equals(m2)); //make sure they are the same again
-		
+	}
+	
+	@Test
+	public final void passCheck() {
+		Provider m1 = new Provider();
+		Provider m2 = new Provider(m1);
+		m2.providedServices.add(new ProvidedServiceRecord(new Date(), "1", "101", "33221", "", 4));
+		m2.providedServices.add(new ProvidedServiceRecord(new Date(), "2", "202", "22334", "", 4));
+		m1.providedServices.add(m2.providedServices.get(1));
+		assertFalse(m1.equals(m2)); //Makes sure it's different
+		m2.providedServices.remove(0);
+		assertTrue(m1.equals(m2)); //Makes sure they are the same after changes
 	}
 
 }
